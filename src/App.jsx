a102dd4bf41b82a5fe5366a910455e5daafa1418
kaahbat <carlos.hns@aluno.ufop.edu.br>
2025-05-27 13:44:19 -0300

    inicio do projeto react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/tasks";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" text-red-500 ">Gereciador de tarefas</h1>
+      <Tasks />
+      <AddTask />
+    </div>
+  );
+  s;
+}
+
+export default App;
